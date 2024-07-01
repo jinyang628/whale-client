6ff84dc94171620af57e64172c4720519f39f96d
jinyang628 <chenjinyang4192@gmail.com>
2024-06-30 17:28:00 -0700

    Update

--- whale_client/models/table.py
+++ whale_client/models/table.py
@@ -2,20 +2,36 @@ from enum import StrEnum
 from pydantic import BaseModel
 from typing import Optional
 
+# TODO: Expand the valid data types as per SQLAlchemy/SQLite's valid data types
 class DataType(StrEnum):
     STRING = "string"
     INTEGER = "integer"
     FLOAT = "float"
     BOOLEAN = "boolean"
-    DATE = "date"
     DATETIME = "datetime"
+    
+# TODO: Add more types, e.g. UUID, etc. Will handle the id generation in house in server
+# TODO: Some of the primary key types require the column to be a particular data type. E.g. AUTO_INCREMENT requires the column to be an integer
+class PrimaryKey(StrEnum):
+    NONE = "none" 
+    AUTO_INCREMENT = "auto_increment"
     
 class Column(BaseModel):
     name: str
     data_type: DataType
+    primary_key: PrimaryKey = PrimaryKey.NONE
     nullable: bool = False
 
 class Table(BaseModel):
     name: str
     description: Optional[str] = None
     columns: list[Column]
+    
+    def __init__(self, **data):
+        super().__init__(**data)
+        self._validate_primary_key()
+
+    def _validate_primary_key(self):
+        primary_key_columns = [col for col in self.columns if col.primary_key != PrimaryKey.NONE]
+        if len(primary_key_columns) != 1:
+            raise ValueError("Exactly one column must be set as primary key.")
--- whale_client/sandbox/main.py
+++ whale_client/sandbox/main.py
@@ -1,12 +1,13 @@
 import asyncio
 from whale_client.models.application import Application
-from whale_client.models.table import Column, DataType, Table
+from whale_client.models.table import Column, DataType, PrimaryKey, Table
 from whale_client.models.manager import Manager
 
 
 async def main():
     
     user_info_columns = [
+        Column(name="id", data_type=DataType.INTEGER, nullable=False, primary_key=PrimaryKey.AUTO_INCREMENT),
         Column(name="name", data_type=DataType.STRING, nullable=False),
         Column(name="email", data_type=DataType.STRING, nullable=True)
     ]
@@ -14,6 +15,7 @@ async def main():
     user_info_table = Table(name="user_info", description="This table stores the user information", columns=user_info_columns)
     
     usage_columns = [
+        Column(name="id", data_type=DataType.INTEGER, nullable=False, primary_key=PrimaryKey.AUTO_INCREMENT),
         Column(name="name", data_type=DataType.STRING, nullable=False),
         Column(name="tokens", data_type=DataType.INTEGER, nullable=False),
     ]
