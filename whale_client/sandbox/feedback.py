bb04ee2652d47b3d0d10529f906bb990efe0e5a8
Aarontanzb <aarontanzb@gmail.com>
2024-07-07 14:29:51 -0700

    create files

---
+++ whale_client/sandbox/feedback.py
@@ -0,0 +1,54 @@
+import asyncio
+from whale_client.models.application import Application
+from whale_client.models.table import Column, DataType, PrimaryKey, Table
+from whale_client.models.manager import Manager
+
+
+async def main():
+
+    user_info_columns = [
+        Column(
+            name="id",
+            data_type=DataType.INTEGER,
+            nullable=False,
+            primary_key=PrimaryKey.AUTO_INCREMENT,
+        ),
+        Column(name="name", data_type=DataType.STRING, nullable=False),
+        Column(name="email", data_type=DataType.STRING, nullable=True),
+    ]
+
+    user_info_table = Table(
+        name="user_info",
+        description="This table stores the user information",
+        columns=user_info_columns,
+    )
+
+    usage_columns = [
+        Column(
+            name="id",
+            data_type=DataType.INTEGER,
+            nullable=False,
+            primary_key=PrimaryKey.AUTO_INCREMENT,
+        ),
+        Column(name="name", data_type=DataType.STRING, nullable=False),
+        Column(name="tokens", data_type=DataType.INTEGER, nullable=False),
+    ]
+
+    usage_table = Table(
+        name="usage",
+        description="This table stores the usage information",
+        columns=usage_columns,
+    )
+
+    user_profile_tables = [user_info_table, usage_table]
+    application_name = "huge_whale"
+
+    application = Application(application_name, user_profile_tables)
+
+    manager = Manager()
+    response: str = manager.commit(application=application)
+    print(response)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
